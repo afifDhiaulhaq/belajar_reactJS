a45d69927188b59ba071a517a2ad96463b8b6c76
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2021-09-08 17:36:33 +0700

    update

---
+++ ecosystem/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { liked: false };
+  }
+
+  render() {
+    let button = <button onClick={() => this.setState({ liked: true }) }>Like</button>;
+
+    if (this.state.liked) {
+      button = 'You liked this.';
+    }
+
+    return (
+      <>
+        <div className="App">
+          <h2>Add React in One Minute</h2>
+          <p>This page demonstrates using React with no build tooling.</p>
+          <p>React is loaded as a script tag.</p>
+          {button}
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
